48a7b59db4beb6d006cce66cc727cd53d72270d4
Sylvain Bonfils <plaisir@odevie.org>
2019-01-24 15:14:44 +0100

    users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,7 @@ module Api
     class UsersController < ApplicationController
 
       before_action :authenticate_user!
+      before_action :set_user, only: [:show, :update, :destroy]
 
       def index
         @user = User.all.as_json(include: :role)
@@ -10,7 +11,26 @@ module Api
       end
 
 
+      def show
+        json_response(@user, api_v1_users_url(@user))
+      end
+
+      def update
+        @user.update(sign_up_params)
+        json_response(@user.as_json(include: :role), api_v1_users_url(@user))
+      end
+
+      def destroy
+        @user.destroy
+        head :no_content
+      end
+
       private
+
+      def set_user
+        # 404 managed by concern exeption handler
+        @user = User.find(params[:id])
+      end
 
       def sign_up_params
         params.require(:user).permit(:email, :password, :password_confirmation, :role_id)
